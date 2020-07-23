64186a2e04f96bb6067d8ff2c2dcf873cb3ee2a0
Antonio Maria Vigliotti <antoniomaria.vigliotti@gmail.com>
2020-07-23 20:43:23 +0200

    [TEST] test

--- midea/models/__init__.py
+++ midea/models/__init__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2018-20 - SHS-AV s.r.l. <https://www.zeroincombenze.it/>
+# Copyright 2016-20 - SHS-AV s.r.l. <https://www.zeroincombenze.it/>
 #
 # Contributions to development, thanks to:
 # * Antonio Maria Vigliotti <antoniomaria.vigliotti@gmail.com>
 #
 # License APGL-3.0 or later (http://www.gnu.org/licenses/agpl).
 #
-from . import midea_no_company
+from . import midea_qci
 from . import midea_table_wco
+
