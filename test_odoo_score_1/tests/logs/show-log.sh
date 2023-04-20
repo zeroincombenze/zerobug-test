e6c207c71c0f65a9d1173ae229dfcb88186869c5
zeroincombenze <info@shs-av.com>
2023-04-20 09:46:28 +0200

    [IMP] test_odoo_score_*: upgrade

--- test_odoo_score_1/tests/logs/show-log.sh
+++ test_odoo_score_1/tests/logs/show-log.sh
@@ -1,1 +1,1 @@
-less -R $(readlink -f $(dirname $0))/test_odoo_score_1_20230419.txt
+less -R $(readlink -f $(dirname $0))/test_odoo_score_1_20230420.txt
---
+++ test_odoo_score_2/egg-info/history.rst
@@ -0,0 +1,4 @@
+10.0.0.1.0 (2023-04-16)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* [REF] Exeperimental use of odoo_score for odoo
